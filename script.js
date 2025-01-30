b4d48e158a0857429ce392428aaba6d26b52c352
Laurentino Jaimes <ljaimes92@gmail.com>
2025-01-30 16:37:29 -0500

    got numbers and ops sent to diff vars with if statment, ops are read on press and clear button clears out the vars and screen

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const buttons = document.querySelectorAll("button");
+const numButtons = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator");
 const enterButton = document.querySelector(".enterButton");
-const numDisplay = document.querySelector("p")
-let equation = {num1: "", operator: "", num2: "2"}
+const clearButton = document.querySelector(".clearButton");
+const numDisplay = document.querySelector("p");
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = "";
 
-buttons.forEach((button) => {
-
+numButtons.forEach((button) => {
     button.addEventListener("click", () => {
         numDisplay.innerHTML = '';
         numDisplay.innerText += button.innerText;
-        equation.num1 += button.innerText;
-        console.log(equation.num1);
-        console.log(array[2+"2'"]);
+        if (operator === "") {
+            num1 += button.innerText;
+        } else {
+            num2 += button.innerText;
+        }
+        console.log(num1, num2, operator);
     });
 });
 
+operators.forEach((op) => {
+    op.addEventListener("click", () => {
+        operator = op.innerText;
+        numDisplay.innerHTML = "";
+        console.log(operator + "q");
+    });
+});
 
+clearButton.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    numDisplay.innerHTML = "";
+    console.log("cleared" + num1, num2, operator);
+});
+
+enterButton.addEventListener("click", () => {
+
+});
